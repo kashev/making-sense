d059c669b55f60c8d9ac68ac443b5131b3e48611
Kashev Dalmia <kashev.dalmia@gmail.com>
2013-12-14 22:16:24 -0600

    wrote simple demo, needs testing.

--- simple_bt_demo/simple_bt_demo.ino
+++ simple_bt_demo/simple_bt_demo.ino
@@ -9,3 +9,52 @@
     simple_bt_demo.ino
 */
 
+#include "Bluetooth.h"
+
+/*
+ * BLUETOOTH
+ */
+#define pinBT_RX 8
+#define pinBT_TX 9
+Bluetooth bt = Bluetooth(pinBT_RX, pinBT_TX);
+
+static int count = 0;
+
+/*
+ * ENERGIA REQUIRED
+ */
+
+void
+setup (void)
+{
+    Serial.begin(9600);
+    bt.begin();
+}
+
+void
+loop (void)
+{
+    char r;
+    while (true)
+    {
+        if (bt.available())
+        {
+            r = bt.read();
+
+            bt.print(r);
+
+            if (r == 'i')
+            {
+                count++;
+            }
+            else if (r == 'd')
+            {
+                count --;
+            }
+
+            bt.print(count);
+            bt.print("\n");
+            bt.print("count : ");
+        }
+    }
+}
